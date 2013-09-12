fa272c4a113291f142d422839c36968ca248ac83
Shantanu <shantanu@cpan.org>
2013-09-12 15:40:26 +0700

    Switched to SHANTANU pluginBundle

--- lib/Acme/CPANAuthors/India.pm
+++ lib/Acme/CPANAuthors/India.pm
@@ -5,11 +5,12 @@ use strict;
 use warnings FATAL => 'all';
 use utf8;
 
-=head1 NAME
+# PODNAME: Acme::CPANAuthors::India 
+# VERSION
+# ABSTRACT: We are the Indian CPAN Authors. Coming from that big peninsula in the center of Asia, the original Indians. The ones columbus was looking for. :)
+# COPYRIGHT
 
-Acme::CPANAuthors::India - We are the CPAN Authors from India!
-
-=cut
+# Dependencies
 
 use Acme::CPANAuthors::Register (
     ADARSHTP      => 'ADARSH TP',
@@ -81,10 +82,13 @@ use Acme::CPANAuthors::Register (
     VIPUL         => 'Vipul Ved Prakash',
 );
 
-=head1 SYNOPSIS
+1;
 
-We are the Indian CPAN Authors. Coming from that big peninsula in the center of Asia, the original Indians. The ones columbus was looking for. :)
+__END__
 
+=begin wikidoc
+
+= SYNOPSIS
 
     use Acme::CPANAuthors;
 
@@ -98,66 +102,21 @@ We are the Indian CPAN Authors. Coming from that big peninsula in the center of 
     my $name     = $authors->name("SHANTANU");
     ...
 
-=head1 DESCRIPTION
+= DESCRIPTION
 
 This Module provides a List of all Indian CPAN Authors Listed. 
 
-=head1 MAINTENANCE
+= NOTES
 
-If you are a Indian CPAN author not listed here, please send your ID/name via email or RT(Link given below) so I can keep this module up to date.
+ * If you are a Indian CPAN author not listed here, please send your ID/name via email or a pull request on github so I can keep this module up to date. 
+ * If you are not a Indian CPAN author but still on the list here, please send me your ID/name via email or submit a pull request on github and I will remove your name.
 
-If you are not a Indian CPAN author but still on the list here, please send me your ID/name via email or RT and I will remove your name.
+= SEE ALSO
 
-=head1 AUTHOR
+ * [Acme::CPANAuthors::Register]
 
-Shantanu Bhadoria, C<< <shantanu aatt cpan dottt org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-acme-cpanauthors-india at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Acme-CPANAuthors-India>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Acme::CPANAuthors::India
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Acme-CPANAuthors-India>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Acme-CPANAuthors-India>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Acme-CPANAuthors-India>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Acme-CPANAuthors-India/>
-
-=back
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2011 Shantanu Bhadoria.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
-
+=end wikidoc
 
 =cut
 
-1; # End of Acme::CPANAuthors::India
+ # End of Acme::CPANAuthors::India
